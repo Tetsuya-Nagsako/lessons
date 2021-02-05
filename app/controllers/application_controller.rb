e38570bddf07610c7d75aac0a776af69617fbe30
tetsuya <nunobokono@gmail.com>
2021-02-05 10:20:52 +0000

    2102051920

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
-      pages_index_path
+      pages_index_path(resource)
   end
   
   def after_sign_up_path_for(resource)
