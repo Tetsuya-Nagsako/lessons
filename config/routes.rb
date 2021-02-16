0a91106a40f464388b094a5dfd5c2eb65ad0bfef
tetsuya <nunobokono@gmail.com>
2021-02-16 05:35:32 +0000

    2102161435button実装

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
       get :user_index
     end
   end
+  
+  post "lessons/:id/charge", to: "charges#create", as: "charge"
+  
   root "top_pages#top"
+  
   get 'top_pages/help'
   get 'pages/index'
   get 'pages/show'
