a578ecba64a26f308c3dbcab51e212b708cdfdbb
tetsuya <nunobokono@gmail.com>
2021-03-03 06:39:30 +0000

    2103031539

--- spec/factories/lessons.rb
+++ spec/factories/lessons.rb
@@ -3,6 +3,7 @@ FactoryBot.define do
     title { "test" }
     description { "test" }
     price { 200 }
+    # image  { Rack::Test::UploadedFile.new(File.join(Rails.root, 'spec/fixtures/logo.png')) }
     time_information { Time.current }
     user
   end
--- spec/models/payment_spec.rb
+++ spec/models/payment_spec.rb
@@ -1,11 +1,15 @@
 require 'rails_helper'
+require 'carrierwave/test/matchers'
 
 RSpec.describe Payment, type: :model do
+  include CarrierWave::Test::Matchers
   describe 'Paymentデータの保守性' do
     before do
       @user = create(:user)
       @lesson = create(:lesson, user: @user)
       @payment = create(:payment, user: @user, lesson: @lesson)
+      let(:image_path) { Rails.root.join('spec/fixtures/logo.png') }
+      let(:image) { Rack::Test::UploadedFile.new(image_path) }
     end
     context 'ユーザーがなくなる場合' do
       it "支払ったユーザーがなくなっても、なくならない" do
