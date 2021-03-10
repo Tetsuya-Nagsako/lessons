a0e0ae9d61ddc0cf1a3323da349269e0f458faa0
tetsuya <nunobokono@gmail.com>
2021-03-10 07:41:42 +0000

    メーラーの設定

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,27 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  
+  
+  if Rails.application.credentials.gmail.present?
+    mail_address = Rails.application.credentials.gmail[:address]
+    password = Rails.application.credentials.gmail[:password]
+  else
+    mail_address = 'admin@example.com'
+    password = 'password'
+  end
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      enable_starttls_auto: true,
+      address: "smtp.gmail.com",
+      port: 587,
+      user_name: mail_address,
+      password: password,
+      authentication: "plain"
+  }
+  
+  
 end
