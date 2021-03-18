f6e44bbad643fd94dbd83b39f91bfea29312caf0
tetsuya <nunobokono@gmail.com>
2021-03-18 08:14:14 +0000

    2103181713挿絵追加、registrations->destroyable?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :user_deletable?, only: %i[ destroy ], if: -> { controller_name == "registrations" }
   
   def after_sign_in_path_for(resource)
       top_pages_home_path
@@ -16,4 +17,11 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit :account_update, keys: added_attrs
     devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
   end
+  
+  def user_deletable?
+    unless current_user.sales == 0 && current_user.lessons.blank?
+      redirect_to root_path, flash: {danger: '売上が残っているか、関係するレッスンが残っているため、アカウント削除できません。'}
+    end
+  end
+  
 end
